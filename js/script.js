b354188c8a74962f9646b036735fc0890e3cd3ce
kellylouise <kellylouisev@gmail.com>
2018-07-12 10:40:45 -0400

    JS updates
    JS updates

--- js/script.js
+++ js/script.js
@@ -1,27 +1,37 @@
+var doCoolStuff = function () {
+	document.getElementById('cool').className = 'cool red';
+}
+
+var sayMyName = function (name) {
+	alert('My name is: '+name);
+}
+
 var car = {	
-	make: 'VW',
-	type: 'Polo',
-	color: 'blue'
-	isTurnedOn: false,
-	numberOfWheels: 4,
-	seats: [
-		'seat 1',
-		'seat 2',
-		'seat 3',
-		'seat 4'
-	],
+			make: 'VW',
+			type: 'Polo',
+			color: 'blue',
+			isTurnedOn: false,
+			numberOfWheels: 4,
+			seats:
+				['seat 1',
+				'seat 2',
+				'seat 3',
+				'seat 4'],
+				
 	turnOn: function (){
 		this.isTurnedOn = true;
 	},
+
 	fly: function (){
 		alert('fly');
 	},
+
 	switchCar: function(isOn) {
-		console.log('turn car' + 'isOn")f
-			;
+		console.log('turn car' + isOn)
 		if (isOn == true){
 			this.isTurnedOn = true;
-		} else {
+		} 
+		else {
 			this.isTurnedOn = false;
 		}				
 	}
